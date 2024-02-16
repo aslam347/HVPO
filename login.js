75dbdd40297864166baba695c0aa59decd73e24a
unknown <aslamthanjavur10@gmail.com>
2024-02-16 01:28:41 -0800

    second commit to reduce

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-
+// or line
 const text = document.createElement("p")
 text.innerHTML = "or"
 text.style.color = "blue"
@@ -6,7 +6,7 @@ text.style.display = "flex"
 text.style.justifyContent = "center"
 
 
-
+// newuser button
 const newUser = document.createElement("button")
 newUser.innerText = "NEW USER"
 newUser.style.color = "green"
@@ -14,19 +14,17 @@ newUser.style.color = "green"
 
 
 
-
+//existing user button
 const existingUser = document.createElement("button")
 existingUser.style.color = "red"
 existingUser.innerHTML = "EXISTING USER"
 
 
- // document.body.appendChild(newUser)
-// document.body.appendChild(existingUser)
-
-
-
+// div element used to append the two login button
 let box = document.createElement('div')
 
+
+// styleing to the login page
 box.style.display = "flex";
 box.style.justifyContent = "center";
 box.style.alignItems = "center";
@@ -35,7 +33,12 @@ box.style.height = "200px"
 box.style.width = "50%"
 box.style.flexDirection = "column"
 box.style.gap = "25px"
+box.style.display =  "flex";
+box.style.justifycontent =  "center";
+box.style.alignitems = "center";
 
+
+// appending process
 box.appendChild(newUser)
 box.appendChild(text)
 box.appendChild(existingUser)
