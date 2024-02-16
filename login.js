84775ba8669688390a705b4dcd390fbffc6c226f
unknown <aslamthanjavur10@gmail.com>
2024-02-16 04:28:20 -0800

    background image upgrade

--- login.js
+++ login.js
@@ -4,40 +4,62 @@ text.innerHTML = "or"
 text.style.color = "blue"
 text.style.display = "flex"
 text.style.justifyContent = "center"
+text.style.fontSize = "25px"
 
 
 // newuser button
 const newUser = document.createElement("button")
 newUser.innerText = "NEW USER"
 newUser.style.color = "green"
+newUser.style.fontSize = "25px"
 
 
 //existing user button
 const existingUser = document.createElement("button")
 existingUser.style.color = "red"
 existingUser.innerHTML = "EXISTING USER"
+existingUser.style.fontSize = "25px"
 
 
 // div element used to append the two login button
 let box = document.createElement('div')
 
 
+
+
+let outer = document.getElementById('full')
+
+
+outer.style.background = "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('loginpage image/background.jpg')";
+outer.style. backgroundImage = "url('loginpage image/background.jpg')";
+outer.style.backgroundSize = "c";
+outer.style.backgroundPosition = "center";
+
 // styleing to the login page
-box.style.display = "flex";
-box.style.justifyContent = "center";
-box.style.alignItems = "center";
-box.style.border = "4px solid black";
-box.style.height = "200px"
-box.style.width = "50%"
+outer.style.display = "flex";
+outer.style.justifyContent = "center";
+outer.style.alignItems = "center";
+outer.style.height = "100vh"
+outer.style.width = "100%"
+
+//outer.style.marginLeft = "300px";
+//outer.style.marginTop = "200px";
+
+
+box.style.display = "flex"
+box.style.justifyContent = "center"
+box.style.alignItems = "center"
 box.style.flexDirection = "column"
-box.style.gap = "25px"
-box.style.display =  "flex";
-box.style.justifycontent =  "center";
-box.style.alignitems = "center";
+box.style.border = "25px solid black"
+box.style.borderRadius = '5em';
+//outer.style.gap = "10px"
+box.style.height = "470px"
+box.style.width = "450px"
 
 
 // appending process
 box.appendChild(newUser)
 box.appendChild(text)
 box.appendChild(existingUser)
-document.body.appendChild(box)
+outer.appendChild(box)
+document.body.appendChild(outer)
